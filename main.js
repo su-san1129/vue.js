c358cd28406c2ec93d2a702e02ec0bd0bfa3ae38
su-san1129 <deviationvalue1129@icloud.com>
2019-07-04 18:23:27 +0900

    watchオプション

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    year: (new Date()).getFullYear()
-  },
-  computed: {
-    isUrudoshi: function(){
-      if ((this.year%4 == 0) && (this.year%100 != 0) || (this.year%400 == 0)){
-        return true;
-      } else{
-        return false;
-      }
-    }
-  }
-});
-
-var demo = new Vue({
-  el: '#demo',
-  data: {
-    show: true
+    message: "",
+    stock: 10
   },
   methods: {
-    //現在時刻を返すメソッド
-    now1: function() {
-      return (new Date()).toLocaleString();
+    //削除ボタンのクリックイベントハンドラ
+    onDeleteItem: function(){
+      this.stock--;
     }
   },
-  computed: {
-    //現在日時を返す算出プロパティ
-    now2: function(){
-      return (new Date()).toLocaleString();
+  watch: {
+    //在庫数が変化したときに呼び出されるハンドラ
+    stock: function(newStock, oldStock) {
+      if (newStock == 0) {
+        this.message = '売り切れ';
+      }
     }
   }
 });
