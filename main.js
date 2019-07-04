5c05a2a27ffb8cb479148558cae5709e0f9ad8f7
su-san1129 <deviationvalue1129@icloud.com>
2019-07-04 17:04:32 +0900

    算出プロパティのキャッシュ

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ var app = new Vue({
     }
   }
 });
+
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    show: true
+  },
+  methods: {
+    //現在時刻を返すメソッド
+    now1: function() {
+      return (new Date()).toLocaleString();
+    }
+  },
+  computed: {
+    //現在日時を返す算出プロパティ
+    now2: function(){
+      return (new Date()).toLocaleString();
+    }
+  }
+});
