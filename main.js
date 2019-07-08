d3198cb20a558dd1ba283185856ec65565984334
su-san1129 <deviationvalue1129@icloud.com>
2019-07-08 20:02:46 +0900

    transitonオプション

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-   category: [] //空の配列を渡す
-  },
-  computed: {
-  		//一件以上選択されている分類を返す算出プロパティ
-  	seletedCategory: function(){
-  		//一件以上選択されている場合、選択された値を連結した文字列を返す
-  		return this.category.length >= 1 ? this.category.join() : '';
-  	}
+   show: true
   }
 });
