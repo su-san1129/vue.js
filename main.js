760a835e98fa3404548d0a7877b6c438d4b26b8f
su-san1129 <deviationvalue1129@icloud.com>
2019-07-08 16:01:20 +0900

    複数選択のセレクトボックスにバインド

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var app = new Vue({
   el: '#app',
   data: {
-   answer: ''
+   category: [] //空の配列を渡す
+  },
+  computed: {
+  		//一件以上選択されている分類を返す算出プロパティ
+  	seletedCategory: function(){
+  		//一件以上選択されている場合、選択された値を連結した文字列を返す
+  		return this.category.length >= 1 ? this.category.join() : '';
+  	}
   }
 });
