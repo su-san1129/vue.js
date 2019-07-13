3b5a8f8268c51d468faf10308ff865530b8d3768
su-san1129 <deviationvalue1129@icloud.com>
2019-07-13 18:03:22 +0900

    Vue.js書き換え

--- main.js
+++ main.js
@@ -1,116 +1,17 @@
-//コンポーネントのルートノード
-var nodeApp = document.querySelector('#app');
-
-//チェックボックスのイベントハンドラを登録
-var nodeCheckbox = nodeApp.querySelectorAll('input[type="checkbox"]');
-nodeCheckbox[0].addEventListener('change', onCheckChanged, false);
-nodeCheckbox[1].addEventListener('change', onCheckChanged, false);
-
-//セレクトボックスのイベントハンドラを登録
-var nodeSelect = nodeApp.querySelector('.sorting');
-nodeSelect.addEventListener('change',onOrderChanged, false);
-
-//初期表示時のノードリスト
-var nodeItemsOrg = nodeApp.querySelectorAll('.item');
-
-//チェック状態変更イベントハンドラ
-function onCheckChanged(event) {
-
-  var nodeItems = nodeApp.querySelectorAll('.item');
-  var nodeCount = nodeApp.querySelector('.count');
-  var count     = nodeItems.length;
-
-  //すべての商品ノードをいったん表示する
-  for (var i=0; i<nodeItems.length; i++) {
-    showNode(nodeItems[i]);
+var app = new Vue({
+  el: '#app',
+  data: {
+    count: 0,
+    showSaleItem: false,
+    showDelvFree: false,
+    sortOrder: 1,
+    products:[
+      { name: 'Michael<br>スマホケース', price: 1580, image: 'images/01.jpg', delv:0, isSale: true },
+      { name: 'Raphael<br>スマホケース', price: 1580, image: 'images/02.jpg', delv:0, isSale: true },
+      { name: 'Gabriel<br>スマホケース', price: 1580, image: 'images/03.jpg', delv:240, isSale: true },
+      { name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv:0, isSale: true },
+      { name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv:0, isSale: false },
+      { name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv:0, isSale: false }
+    ]
   }
-
-  //セール対象のチェックがついている場合
-  if (nodeCheckbox[0].checked){
-    //すべての商品のノードを探す
-    for (var i=0; i<nodeItems.length; i++){
-      //セール対象の商品ではない場合
-      if (!isSaleItem(nodeItems[i])){
-        //この商品を非表示にする
-        hideNode(nodeItems[i]);
-        //件数のカウントを減らす
-        count--;
-      }
-    }
-  }
-
-  // 送料無料のチェックがついている場合
-  if (nodeCheckbox[1].checked){
-    //すべての商品ノードを探す
-    for (var i=0; i<nodeItems.length; i++) {
-      //送料無料の商品ではない場合
-      if (!isDelvFreeItem(nodeItems[i])) {
-        //この商品を非表示にする
-        hideNode(nodeItems[i]);
-        count--;
-      }
-    }
-  }
-  //件数を更新
-  nodeCount.textContent = count + '件';
-}
-
-//並び順変更イベントハンドラ
-function onOrderChanged(event){
-
-  var nodeList = nodeApp.querySelector('.list');
-  var nodeItems = nodeApp.querySelectorAll('.item');
-
-  //商品ノードのリストを新しい配列に詰め替える（退散しておく）
-  var products = [];
-  for (var i=0; i<nodeItems.length; i++) {
-    products.push(nodeItems[i]);
-  }
-
-  //Domからすべての商品ｎノードを削除する
-  while (nodeList.firstChild){
-    nodeList.removeChild(nodeList.firstChild);
-  }
-
-  //標準が選択されている場合
-  if (event.target.value == '1'){
-    for (var i=0; i<products.length; i++){
-      nodeList.appendChild(nodeItemsOrg[i]);
-    }
-  }
-
-  else if (event.target.value == '2') {
-    products.sort(function(a,b){
-      var prevPrice = parseInt(a.querySelector('.price span').textContent.replace(',',''));
-      var currentPrice = parseInt(b.querySelector('.price span').textContent.replace(',',''));
-      return prevPrice - currentPrice;
-    });
-
-    //並び替え後の商品ノードをDOMに追加する。
-    for (var i=0; i<products.length; i++){
-      nodeList.appendChild(products[i]);
-    }
-  }
-}
-
-//セール商品かどうかを判定する関数
-function isSaleItem(nodeItem) {
-  var node = nodeItem.querySelector('.status');
-  return (node && node.textContent == 'SALE');
-}
-
-//送料無料かどうかを判定する関数
-function isDelvFreeItem(nodeItem){
-  var node = nodeItem.querySelector('.shipping-fee');
-  return (node && node.textContent == '送料無料');
-}
-
-//ノードを非表示にする関数
-function hideNode(node){
-  node.setAttribute('style','display:none;');
-}
-
-//ノードを表示する関数
-function showNode(node) {
-  node.removeAttribute('style');
-}
+});
